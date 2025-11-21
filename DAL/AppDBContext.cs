195eef66f7fe87769c7d7a26f3e2198bd719f1dd
mattys1 <mattys0082@gmail.com>
2025-11-21 17:20:13 +0100

    add avatar path to model

--- DAL/AppDBContext.cs
+++ DAL/AppDBContext.cs
@@ -1,11 +1,24 @@
+using CSharpFunctionalExtensions;
 using raptorSlot.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
-namespace raptorSlot.DAL
-{
-    public class AppDBContext(DbContextOptions<AppDBContext> options) : IdentityDbContext<AppUser>(options)
-    {
+namespace raptorSlot.DAL {
+	public class AppDBContext(DbContextOptions<AppDBContext> options) : IdentityDbContext<AppUser>(options) {
 
-    }
+		protected override void OnModelCreating(ModelBuilder modelBuilder) {
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Entity<AppUser>()
+				.Property(e => e.AvatarUri)
+				.HasConversion(
+				m => m.HasValue
+					     ? m.Value.Path
+					     : null,
+				s => s != null
+					     ? Maybe.From(new AvatarPath(s))
+					     : Maybe.None
+				);
+		}
+	}
 }
--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -1,9 +1,24 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using CSharpFunctionalExtensions;
 using Microsoft.AspNetCore.Identity;
 
 namespace raptorSlot.Models {
+	public readonly struct AvatarPath {
+		public AvatarPath(Guid userId) {
+			Path = $"/avatars/{userId}.png";
+		}
+
+		public AvatarPath(string avatarPath) {
+			Path = avatarPath;
+		}
+
+		public readonly string Path;
+	}	
+	
 	public class AppUser : IdentityUser {
 
         public int Tokens { get; set; } = 0;
         public int SuperTokens { get; set; } = 0;
-    }
+        public Maybe<AvatarPath> AvatarUri { get; set; }= Maybe.None;
+	}
 }
