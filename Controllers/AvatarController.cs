feaad3e1d0939a84076ba888ac0b22900d5d9ab9
mattys1 <mattys0082@gmail.com>
2025-11-22 20:06:00 +0100

    upload visible on the frontend

--- Controllers/AvatarController.cs
+++ Controllers/AvatarController.cs
@@ -1,10 +1,15 @@
+using System.Net;
+using System.Security.Claims;
+using CSharpFunctionalExtensions;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using raptorSlot.Models;
 using raptorSlot.Services;
 using raptorSlot.Util;
+using SixLabors.ImageSharp.Formats.Png;
 
 namespace raptorSlot.Controllers {
 	[Authorize]
@@ -18,7 +23,33 @@ namespace raptorSlot.Controllers {
 			
 			return View(user);
 		}
-		
+
+		[HttpGet]
+		public async Task<IActionResult> GetSelfAvatar() {
+			var user = await userManager.GetUserAsync(User);
+			if (user == null) {
+				return BadRequest();
+			}
+
+			var avatarResult = await avatarService.GetUserAvatar(user);
+			if (avatarResult.IsFailure) {
+				ModelState.AddModelError(string.Empty, avatarResult.Error);
+				return NotFound();
+			}
+
+			var maybeImage = avatarResult.Value;
+			if (maybeImage.HasNoValue) {
+				return NotFound();
+			}
+
+			var image = maybeImage.Value;
+
+			using var ms = new MemoryStream();
+			image.Save(ms, new PngEncoder());
+			ms.Position = 0;
+
+			return File(ms.ToArray(), "image/png");
+		}
 		[HttpPost]
 		public async Task<IActionResult> DeleteAvatar() {
 			var user = await userManager.GetUserAsync(User);
@@ -57,7 +88,7 @@ namespace raptorSlot.Controllers {
 			var loaded = await SixLabors.ImageSharp.Image.LoadAsync(stream);
 
 			var result = await avatarService.SetUserAvatar(user, loaded);
-			if(result.IsFailure){
+			if(result.IsFailure) {
 				ModelState.AddModelError(string.Empty, result.Error);
 				
 				return View(nameof(Index), user);
